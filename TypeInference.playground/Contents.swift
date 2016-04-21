28bb983c43d382154cd63f8bfe37a2c6cc7c6a42
LauraKirby <laurakirby26@gmail.com>
2016-04-20 18:36:49 -0700

    fix typos, add intro to how I envision the layout and teaching technique, clarify text in experiment section

--- TypeInference.playground/Contents.swift
+++ TypeInference.playground/Contents.swift
@@ -1,37 +1,49 @@
+/* 
+Creater notes:
 
-//: ![Build icon](build_sm.png)
-// INTRO: Swyft is safe, clean and fast.
-
-/*:
-WHY SHOULD I CARE - bc it's safe & clean
-Similar to Objective-C, Swift requires each type to be declared. By explicitly declaring the type our codebase is less prone to crashing further on in the development process because we (and the compiler) know what type to expect. For example, the complier will crash if we try to store a `String` in previously declared type `Double`. By assigning the type ahead of time, the complier is able to allocate
-
-HOW - complier infers the type
-In contrast to Objective-C, type can be inferred. When a variable or a constant is created with a value, the complier can infer the type. Swift's type inference allows us to keep a cleaner codebase by decreasing the need to explicitly declare types.
+ The layout of the documentation will consist of three different sections, 1) Build; 2) Caution; and 3) Experiment. Each section will be denoted by a small icon just to the left of the text within the first paragraph (Due to markup restrictions, images below are not placed in correct location). There will be a key at the beginning of the documentation to outline each section. The seemingly tactile "build" and "experiment" icons will allow the user to see onself as a true engineer aka "Yay, I love learning Swift!" Many people learn better when through the use of images and text, the icons will allow us to eliminate pieces of text that can be easily conveyed through an image throughout the documentation.
  
-EXAMPLE - type inference
- The compiler will automatically declare the variable `drought` as type `String`.
+ Examples given throughout the documentation are provided on a variety of topics, which allows people from diverse backgrounds to relate to the material.
  */
 
+//: ![Build icon](build_sm.png)
+//: INTRO: Swyft is safe, clean and fast.
+
+/*:
+ WHY SHOULD I CARE - bc it's safe & clean
+ 
+ Similar to Objective-C, Swift requires each type to be declared. By explicitly declaring the type our codebase is less prone to crashing further on in the development process because we (and the compiler) know what type to expect. For example, the complier will crash if we try to store a `String` in previously declared type `Double`. By assigning the type ahead of time, the complier is able to allocate
+ 
+ HOW - complier infers the type
+ 
+ In contrast to Objective-C, type can be inferred. When a variable or a constant is created with a value, the complier can infer the type. Swift's type inference allows us to keep a cleaner codebase by decreasing the need to explicitly declare types.
+ 
+ EXAMPLE - type inference
+ 
+ The compiler will automatically declare the variable `drought` as type `String`.
+*/
  var drought = "a prolonged period of abnormally low rainfall; a shortage of water resulting from this"
  
- /*:
-CAUTION - keep code readable
-![Caution icon](caution_sm.png)Type inference allows us to write less code; however, keep in mind that readable code is an asset to our source code. If we assign the value of variable to the result of a function being called, we may have a hard time remembering the type of this variable. In scenarios such as these, let's go ahead and explicitly state the type.
-
-EXPIERMENT - show how delcaring a type can be helpful.
- ![Expierment icon](expierment_sm.png)
+/*:
+ CAUTION - keep code readable
+ 
+ ![Caution icon](caution_sm.png)Type inference allows us to write less code; however, keep in mind that readable code is an asset to our source code. If we assign the value of variable to the result of a function being called, we may have a hard time remembering the type of this variable. In scenarios such as these, let's go ahead and explicitly state the type.
+ 
+ EXPERIMENT - show how delcaring a type can be helpful.
+ 
+ ![Experiment icon](experiment_sm.png)
  Let's say we would like store the year and month a new drought began in California.
 
-We might store the data as 201301 meaning the 1st month in 2013 or we might store the data as 2013.01. Both have equivalent meaning but are not the same types. Further down in our codebase we may forget which type we chose. To make our code more readable, let's explicitly declare the type.
-
-*/
-
+ To keep our numbers in chronolocial order, we might store start date as 201301 or Double 2013.01, both of which could be interpreted to mean the 1st month in 2013. We can see how these two numbers may have equivalent meaning; however, they are different types. 
+ */
 func findDroughtDate() -> Double {
     let year = 2013.0
     let month = 0.01
     return month + year
 }
 
+/*:
+ Further down in our source code, when we are ready to store the returned value or implement a variable which accesses that value, we may forget which type the function returns or the variable holds. To increase readablity, let's explicitly declare the type when the variable is created.
+*/
 let caDrought: Double = findDroughtDate()
 
--- outline_and_sources.md
+++ outline_and_sources.md
@@ -1,6 +1,11 @@
-- implementation - hammer
-- caution - warning sign
-- experiment - beaker
+sources:
+1. https://www.quora.com/What-is-the-difference-between-runtime-and-compile-time
+2. http://books.aidanf.net/learn-swift/types_and_type_inference
+3. https://www.andrewcbancroft.com/2014/08/12/clean-coding-in-swift-type-inference/
+
+- implementation - hammer image
+- caution - warning sign image
+- experiment - beaker image
 
 INTRO:
 Swyft is minimal, safe and fast.
@@ -33,11 +38,3 @@ One item that makes Syft so safe is due to that fact that is it “type safe” 
 Swyft is a “complied language” it is converted to machine code just after the code is written.   as the source code is written it is fed into a complier (another program), which produces an executable program (or an error).
 
 sometimes the easiest way to get started on complex topics is to review the original English definition of the word being used, Complie means to produce (something, especially a list, report, or book) by assembling information collected from other sources. The complier collects the source code (including any libaries or api’s that have been included) and as it is written and uses a secondary program to produce machine readable and executable code
-
-
-sources:
-1. https://www.quora.com/What-is-the-difference-between-runtime-and-compile-time
-
-2. http://books.aidanf.net/learn-swift/types_and_type_inference
-
-3. https://www.andrewcbancroft.com/2014/08/12/clean-coding-in-swift-type-inference/
