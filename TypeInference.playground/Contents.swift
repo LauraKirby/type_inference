de58769d5173ce085c36fa1a6017bbb0ccff8c84
LauraKirby <laurakirby26@gmail.com>
2016-04-20 18:40:43 -0700

    fix more typos

--- TypeInference.playground/Contents.swift
+++ TypeInference.playground/Contents.swift
@@ -1,5 +1,5 @@
 /* 
-Creater notes:
+Creator notes:
 
  The layout of the documentation will consist of three different sections, 1) Build; 2) Caution; and 3) Experiment. Each section will be denoted by a small icon just to the left of the text within the first paragraph (Due to markup restrictions, images below are not placed in correct location). There will be a key at the beginning of the documentation to outline each section. The seemingly tactile "build" and "experiment" icons will allow the user to see onself as a true engineer aka "Yay, I love learning Swift!" Many people learn better when through the use of images and text, the icons will allow us to eliminate pieces of text that can be easily conveyed through an image throughout the documentation.
  
@@ -7,7 +7,7 @@ Creater notes:
  */
 
 //: ![Build icon](build_sm.png)
-//: INTRO: Swyft is safe, clean and fast.
+//: INTRO: Swift is safe, clean and fast.
 
 /*:
  WHY SHOULD I CARE - bc it's safe & clean
@@ -29,12 +29,12 @@ Creater notes:
  
  ![Caution icon](caution_sm.png)Type inference allows us to write less code; however, keep in mind that readable code is an asset to our source code. If we assign the value of variable to the result of a function being called, we may have a hard time remembering the type of this variable. In scenarios such as these, let's go ahead and explicitly state the type.
  
- EXPERIMENT - show how delcaring a type can be helpful.
+ EXPERIMENT - show how declaring a type can be helpful.
  
  ![Experiment icon](experiment_sm.png)
  Let's say we would like store the year and month a new drought began in California.
 
- To keep our numbers in chronolocial order, we might store start date as 201301 or Double 2013.01, both of which could be interpreted to mean the 1st month in 2013. We can see how these two numbers may have equivalent meaning; however, they are different types. 
+ To keep our numbers in chronological order, we might store start date as 201301 or Double 2013.01, both of which could be interpreted to mean the 1st month in 2013. We can see how these two numbers may have equivalent meaning; however, they are different types.
  */
 func findDroughtDate() -> Double {
     let year = 2013.0
@@ -43,7 +43,7 @@ func findDroughtDate() -> Double {
 }
 
 /*:
- Further down in our source code, when we are ready to store the returned value or implement a variable which accesses that value, we may forget which type the function returns or the variable holds. To increase readablity, let's explicitly declare the type when the variable is created.
+ Further down in our source code, when we are ready to store the returned value or implement a variable which accesses that value, we may forget which type the function returns or the variable holds. To increase readability, let's explicitly declare the type when the variable is created.
 */
 let caDrought: Double = findDroughtDate()
 
